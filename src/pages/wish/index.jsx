f691f3fabc619346b4bfd12597b113fd6526dab0
wenshan <hou_ve@hotmail.com>
2024-05-18 17:03:00 +0800

    fix: icp

--- src/pages/wish/index.jsx
+++ src/pages/wish/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'umi';
 import { Empty, Dialog, Modal } from 'antd-mobile';
-import { CheckCircleOutline, CloseCircleOutline} from 'antd-mobile-icons';
 
 import './index.less';
 
